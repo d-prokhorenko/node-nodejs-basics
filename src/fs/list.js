c24b4433683dc49e051f5620c9fd61c9212249cb
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 15:02:28 +0300

    feat: solve fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetDirName = 'files';
+const targetDirPath = path.join(__dirname, targetDirName);
+const errorMessage = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir(targetDirPath, (err, files) => {
+    if (err) throw new Error(errorMessage);
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
 };
 
 await list();
