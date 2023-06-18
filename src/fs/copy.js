5ac947ff6ccc945f4ff90d0cf6d23064aa42c4d5
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 14:34:23 +0300

    feat: solve fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetDirNamePath = path.join(__dirname, 'files');
+const copiedDirName = path.join(__dirname, 'files_copy');
+const errorMessage = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+  fs.stat(targetDirNamePath, fs.F_OK, (err) => {
+    if (err) {
+      throw new Error(errorMessage);
+    } else {
+      fs.mkdir(copiedDirName, (err) => {
+        if (err) throw new Error(errorMessage);
+        fs.cp(targetDirNamePath, copiedDirName, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
+      });
+    }
+  });
 };
 
 await copy();
