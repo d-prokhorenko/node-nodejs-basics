05e605999a24e89ba1f0a3ef76dc9af94dd1ca75
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 19:36:55 +0300

    feat: solve streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const { stdout } = process;
+const __dirname = getDirName(import.meta.url);
+
+const targetFileName = 'fileToRead.txt';
+const targetDirName = 'files';
+const targetFilePath = path.join(__dirname, targetDirName, targetFileName);
+
 const read = async () => {
-    // Write your code here 
+  fs.createReadStream(targetFilePath, 'utf-8').on('data', (chunk) =>
+    stdout.write(chunk)
+  );
 };
 
 await read();
