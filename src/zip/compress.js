883abf91a3ae13dcf7f6a00913836ca20b548d55
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 20:06:28 +0300

    feat: solve zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import { promisify } from 'util';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetFileName = 'fileToCompress.txt';
+const compressedFileName = 'archive.gz';
+const targetDirName = 'files';
+const targetFilePath = path.join(__dirname, targetDirName, targetFileName);
+const compressedFilePath = path.join(
+  __dirname,
+  targetDirName,
+  compressedFileName
+);
+
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(targetFilePath);
+  const destination = createWriteStream(compressedFilePath);
+  await pipe(source, gzip, destination);
 };
 
-await compress();
+await compress().catch((err) => {
+  console.error('An error occurred:', err);
+  process.exitCode = 1;
+});
