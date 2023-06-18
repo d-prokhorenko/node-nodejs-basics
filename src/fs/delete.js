79c6d3b76bc34b068f3f343f41544d4d072a1d3f
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 14:55:36 +0300

    feat: solve fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetDirName = 'files';
+const targetFileName = 'fileToRemove.txt';
+const targetFilePath = path.join(__dirname, targetDirName, targetFileName);
+const errorMessage = 'FS operation failed';
+const successfulMessage = 'The file has been deleted';
+
 const remove = async () => {
-    // Write your code here 
+  fs.access(targetFilePath, fs.F_OK, (err) => {
+    if (err) throw new Error(errorMessage);
+    fs.unlink(targetFilePath, (err) => {
+      if (err) throw err;
+      console.log(successfulMessage);
+    });
+  });
 };
 
 await remove();
