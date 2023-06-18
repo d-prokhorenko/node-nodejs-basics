a586a3fd4c5efb6c37373a71c9173a93aa91553c
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 19:44:42 +0300

    feat: solve streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const { stdin } = process;
+const __dirname = getDirName(import.meta.url);
+
+const targetFileName = 'fileToWrite.txt';
+const targetDirName = 'files';
+const targetFilePath = path.join(__dirname, targetDirName, targetFileName);
+
 const write = async () => {
-    // Write your code here 
+  stdin.on('data', (data) => {
+    fs.createWriteStream(targetFilePath).write(data);
+  });
 };
 
 await write();
