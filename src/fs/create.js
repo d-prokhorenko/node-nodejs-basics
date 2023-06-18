302499b5b5e6cb1078120c0cabab99254e01bb39
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 13:59:54 +0300

    feat: solve fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetDirName = 'files';
+const targetFileName = 'fresh.txt';
+const targetFileContent = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+const successfulMessage = 'The file has been created';
+
 const create = async () => {
-    // Write your code here 
+  fs.access(
+    path.join(__dirname, targetDirName, targetFileName),
+    fs.F_OK,
+    (err) => {
+      if (!err) {
+        console.error(errorMessage);
+      } else {
+        fs.writeFile(
+          path.join(__dirname, targetDirName, targetFileName),
+          targetFileContent,
+          (err) => {
+            if (err) throw err;
+            console.log(successfulMessage);
+          }
+        );
+      }
+    }
+  );
 };
 
 await create();
