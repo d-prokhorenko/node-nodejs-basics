47bf13fc46f46ceb5cc89954d47c6a38cb11584d
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 18:06:22 +0300

    feat: solve cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+const vars = process.env;
+const RSS_KEY = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+  const parsedVarsKeys = Object.keys(vars).filter((varKey) =>
+    varKey.includes(RSS_KEY)
+  );
+  const result = parsedVarsKeys.reduce(
+    (res, varKey, index) =>
+      `${res}${varKey}=${vars[varKey]}${
+        index === parsedVarsKeys.length - 1 ? '' : '; '
+      }`,
+    ''
+  );
+  console.log(result);
 };
 
 parseEnv();
