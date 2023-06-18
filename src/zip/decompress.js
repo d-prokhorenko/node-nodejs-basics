8db9d236d66b3544cdb54d5eade34f9053a8f39b
d-prokhorenko <denis.prokhorenko1@gmail.com>
2023-06-18 20:15:34 +0300

    featu: solve zip/decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import * as path from 'path';
+import { getDirName } from '../utils/functions.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const targetFileName = 'archive.gz';
+const compressedFileName = 'fileToCompress.txt';
+const targetDirName = 'files';
+const targetFilePath = path.join(__dirname, targetDirName, targetFileName);
+const compressedFilePath = path.join(
+  __dirname,
+  targetDirName,
+  compressedFileName
+);
+
 const decompress = async () => {
-    // Write your code here 
+  const fileContents = createReadStream(targetFilePath);
+  const writeStream = createWriteStream(compressedFilePath);
+  const unzip = createGunzip();
+
+  fileContents.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
